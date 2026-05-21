522c480dcbc5cb362db680d539d8bfb60abcd3f4
MSI <MSI@DESKTOP-CE6KTT9>
2026-05-22 00:16:56 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Models/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HabitTrackerWPF.Models
+{
+    public class UserProfile
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Password { get; set; } = "";
+        public DateTime BirthDate { get; set; } = DateTime.Now;
+        public string Education { get; set; } = "Среднее";
+        public List<string> Hobbies { get; set; } = new List<string>();
+        public bool ReceiveNotifications { get; set; } = false;
+        public bool PublicStats { get; set; } = false;
+        public bool AutoSave { get; set; } = true;
+        public string ActivityLevel { get; set; } = "Средний";
+    }
+}
